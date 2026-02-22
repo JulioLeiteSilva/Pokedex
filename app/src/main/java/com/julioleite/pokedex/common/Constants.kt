fd7c4964cc4b1b50a0589a21dfe56fc6b3eec5b7
julio <julioleite93d@gmail.com>
2026-02-22 16:01:08 -0300

    feat: create constants and resource files

---
+++ app/src/main/java/com/julioleite/pokedex/common/Constants.kt
@@ -0,0 +1,9 @@
+package com.julioleite.pokedex.common
+
+object Constants {
+    const val BASE_URL = "https://pokeapi.co/api/v2/"
+
+    const val IMAGE_URL_BASE = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/"
+
+    const val PAGE_SIZE = 20
+}
